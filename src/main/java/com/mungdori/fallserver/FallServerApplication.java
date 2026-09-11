f4e0d1c9778a13acf03c8dc83ae90e512b38983f
mungdori <mungdori999@gmail.com>
2026-09-11 18:15:22 +0900

    첫번째 커밋

---
+++ src/main/java/com/mungdori/fallserver/FallServerApplication.java
@@ -0,0 +1,13 @@
+package com.mungdori.fallserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FallServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FallServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mungdori/fallserver/FallServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mungdori.fallserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FallServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
